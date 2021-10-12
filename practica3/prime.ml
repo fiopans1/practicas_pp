9dc6815f23da0dcf9a9d784e5522030f80104c69
fiopans1 <dsuaran@gmail.com>
2021-10-13 00:10:15 +0200

    se esta mejorando isprime2

--- practica3/prime.ml
+++ practica3/prime.ml
@@ -5,21 +5,15 @@ i >= n ||
 (n mod i <> 0 && check_from (i+1))
 in check_from 2;;
 
-(*let is_prime2 n =
-let rec check_from i =
-i >= sqrt(n) ||
-(n mod. i <> 0 && check_from (i+1))
-in check_from 2;;*)
+let is_prime2 n =
+let rec check_from1 i =
+i >= int_of_float(sqrt(float_of_int(n))) ||
+(int_of_float(sqrt(float_of_int(n))) mod i <> 0 && check_from1 (i+1))
+in check_from1 2;;
 
 
-let rec next_prime z = let is_prime n =
-let rec check_from i =
-i >= n ||
-(n mod i <> 0 && check_from (i+1))
-in check_from 2 in if is_prime(z)=true then print_int(z) else next_prime(z+1);;
+let rec next_prime z =
+  if is_prime(z)=true then z else next_prime(z+1);;
 
-let rec last_prime_to z = let is_prime n =
-let rec check_from i =
-i >= n ||
-(n mod i <> 0 && check_from (i+1))
-in check_from 2 in if (is_prime(z)=true && z>0) then print_int(z) else last_prime_to(z-1);;
+let rec last_prime_to z =
+  if (is_prime(z)=true && z>0) then z else last_prime_to(z-1);;
