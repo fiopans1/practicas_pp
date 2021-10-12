a6c73597ff531b35add7087bb4fa4a06b2f186ca
fiopans1 <dsuaran@gmail.com>
2021-10-13 00:30:16 +0200

    prime2 terminado

--- practica3/prime.ml
+++ practica3/prime.ml
@@ -5,11 +5,15 @@ i >= n ||
 (n mod i <> 0 && check_from (i+1))
 in check_from 2;;
 
-let is_prime2 n =
+let is_p n =
 let rec check_from1 i =
-i >= int_of_float(sqrt(float_of_int(n))) ||
-(int_of_float(sqrt(float_of_int(n))) mod i <> 0 && check_from1 (i+1))
-in check_from1 2;;
+  i >= n ||
+  (n mod i <> 0 && check_from1 (i+2))
+  in check_from1 3;;
+
+let is_prime2 n =
+  if n=2 then true else(
+  if n mod 2 = 0 then false else (is_p n));;
 
 
 let rec next_prime z =
