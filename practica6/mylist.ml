a4d5f3a4c86671ea4b23b1b07382dcc7f3c41cf6
fiopans1 <dsuaran@gmail.com>
2021-10-18 10:20:28 +0200

    dsad

--- practica6/mylist.ml
+++ practica6/mylist.ml
@@ -34,3 +34,32 @@ let rec append l1 l2= match l1 with
 let rec find f l= match l with
     []->raise(Failure "Ningun elemento cumple")
     | h::t-> if(f h) then h else find f t;;
+(**************************************************************)
+let rec for_alli f l= match l with
+    []->true
+    | h::t-> if(f h) then (for_alli f t) else false;;
+let for_all f l= match l with
+    []->raise Not_found
+    | h::t-> (for_alli f l);;
+(**************************************************************)
+let rec exists f l= match l with
+    []->false
+    | h::t-> if(f h) then true else (exists f t);;
+(**************************************************************)
+let rec mem n l= match l with
+  []->false
+  | h::t->if(h=n) then true else (mem n t);;
+(**************************************************************)
+let rec fil f l l1= match l with
+  []->l1
+  | h::t->if(f h) then (fil f t (append l1 [h])) else (fil f t l1);;
+let filter f l= (fil f l []);;
+(**************************************************************)
+let find_all f l= filter f l;;
+(**************************************************************)
+let rec parti f l l1 l2= match l with
+  []->(l1,l2)
+  | h::t-> if(f h) then (parti f t (append h l1) l2) else (parti f t l1 (append h l2));;
+let partition f l= match l with
+  []->raise(Failure "partition")
+  | h::t-> parti f l [] [];;
