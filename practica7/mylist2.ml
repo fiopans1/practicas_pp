e3aea53e0bca3bec8b273064bd2db529babd3cdd
fiopans1 <dsuaran@gmail.com>
2021-10-25 10:00:59 +0200

    commit

--- practica7/mylist2.ml
+++ practica7/mylist2.ml
@@ -15,9 +15,11 @@ let rec len l n= match l with
 let length l=
         len l 0;;
 (************************************************************)
-let compare_lengths l1 l2=
-  let (x,y)=((length l1), (length l2)) in
-    if x>y then 1 else (if x<y then -1 else 0);;
+let rec compare_lengths l1 l2=match l1,l2 with
+  [],h::t->(-1)
+  | h::t,[]->1
+  | [],[]->0
+  | _::t,_::t2-> compare_lengths t t2;;
 (************************************************************)
 let rec nthi l n p= 
   match l with
@@ -32,7 +34,7 @@ let rec append l1 l2= match l1 with
   | h::t -> h::(append t l2);;
 (**************************************************************)
 let rec find f l= match l with
-    []->raise(Failure "Ningun elemento cumple")
+    []->raise(Not_found)
     | h::t-> if(f h) then h else find f t;;
 (**************************************************************)
 let rec for_alli f l= match l with
@@ -51,59 +53,61 @@ let rec mem n l= match l with
   | h::t->if(h=n) then true else (mem n t);;
 (**************************************************************)
 let rec fil f l l1= match l with
-  []->l1
-  | h::t->if(f h) then (fil f t (append l1 [h])) else (fil f t l1);;
+  []->rev(l1)
+  | h::t->if(f h) then (fil f t (h::l1)) else (fil f t l1);;
 let filter f l= (fil f l []);;
 (**************************************************************)
 let find_all f l= filter f l;;
 (**************************************************************)
 let rec parti f l l1 l2= match l with
-  []->(l1,l2)
-  | h::t-> if(f h) then (parti f t (append l1 [h]) l2) else (parti f t l1 (append l2 [h]));;
+  []->(rev(l1),rev(l2))
+  | h::t-> if(f h) then (parti f t (h::l1) l2) else (parti f t l1 (h::l2));;
 let partition f l= match l with
   []->raise(Failure "partition")
   | h::t-> parti f l [] [];;
 (**************************************************************)
 let rec spl l l1 l2= match l with
-  []->(l1,l2)
-  | h::t-> spl t (append l1 [fst(h)]) (append l2 [snd(h)]);;
+  []->(rev(l1),rev(l2))
+  | h::t-> spl t (fst(h)::l1) (snd(h)::l2);;
 let split l= spl l [] [];;
 (**************************************************************)
 let rec combi l1 l2 l3= match l1,l2 with
-  [],[]->l3
-  | h::t,h1::t1-> combi t t1 (append l3 [(h,h1)]);;
+  [],[]->rev(l3)
+  | h::t,h1::t1-> combi t t1 ((h,h1)::l3);;
 let combine l1 l2= 
-  if((length l1)=(length l2)) then (combi l1 l2 []) else raise(Failure "combine");;
+  if((length l1)=(length l2)) then (combi l1 l2 []) else raise(Invalid_argument "combine");;
 (**************************************************************)
 let rec ini f n p l1=
-  if(p<n) then ini f n (p+1) (append l1 [(f p)]) else  l1;;  
-let init n f= ini f n 0 [];; 
+  if(p<n) then ini f n (p+1) ((f p)::l1) else  rev(l1);;  
+let init n f= if(n<0) then raise(Invalid_argument "init") else (ini f n 0 []);; 
 (**************************************************************)
 let rec re l l1= match l with
   []->l1
   | h::t->re t (append [h] l1);;
 let rev l= re l [];;
 (**************************************************************)
-let rev_append l1 l2= append (rev l1) l2;;
+let rec rev_append l1 l2= match l1 with
+  []->l2
+  | h::t->rev_append t (h::l2);;
 (**************************************************************)
 let rec conca l1 l2= match l1 with
-  []->l2
-  | h::t->conca t (append l2 h);;
+  []->rev(l2)
+  | h::t->conca t (h::l2);;
 
 let concat l= conca l [];;
 (**************************************************************)
-let flatten= concat l;;
+let flatten l= concat l;;
 (**************************************************************)
 let rec ma f l l1= match l with
-  []->l1
-  | h::t -> ma f t (append l1 [(f h)]);;
+  []->rev(l1)
+  | h::t -> ma f t ((f h)::l1);;
 let map f l= ma f l [];;
 (**************************************************************)
 let rev_map f l= map f (rev l);;
 (**************************************************************)
 let rec ma1 f l1 l2 l3= match l1,l2 with
-  [],[]->l3
-  | h::t,h1::t1-> ma1 f t t1 (append  l3 [(f h h1)]);;
+  [],[]->rev(l3)
+  | h::t,h1::t1-> ma1 f t t1 ((f h h1)::l3);;
 let map2 f l1 l2=
   if((length l1)= (length l2)) then (ma1 f l1 l2 []) else  raise(Failure "map2");;
 (**************************************************************)
@@ -115,10 +119,3 @@ let map2 f l1 l2=
 let rec fold_right f l b = match l with
   [] -> b
   | h::t -> f h (fold_right f t b);;
-
-
-
-
-
-
-
