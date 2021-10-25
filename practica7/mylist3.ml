af08d29a64a58d79838440c69a8fabfec545509e
fiopans1 <dsuaran@gmail.com>
2021-10-25 10:23:41 +0200

    das

---
+++ practica7/mylist3.ml
@@ -0,0 +1,26 @@
+(*DIEGO SUAREZ RAMOS- diego.suarez.ramos@udc.es*)
+let rec remove n l= match l with
+  [] -> []
+  | h::t -> if (n = h) then t else h::(remove n t);;
+(************************************************************)
+  let rec remove_all n l= match l with
+  [] -> []
+  | h::t -> if (n = h) then (remove n t) else h::(remove n t);;
+  (************************************************************)
+  let rec ldif1 l1 l2 l3= match l1 with
+  []->l3
+  | h::t->(if (List.mem h l2) then (ldif1 t l2 (remove_all h l3)) else (ldif1 t l2 l3));;
+  let ldif l1 l2= ldif1 l1 l2 l1;;
+    (************************************************************)
+    let rec lprod l1 l2 = match l1 with
+  [] -> []
+  | h::t ->
+    let rec aux h1 l2 = match l2 with
+      [] -> []
+      | h::t -> (h1, h)::(aux h1 t)
+    in List.append (aux h l2) (lprod t l2);;
+   (************************************************************)
+let rec divide = function
+  h1::h2::t -> let  l1, l2 = divide t
+    in h1::l1,h2::l2
+  | l -> l, [];;
