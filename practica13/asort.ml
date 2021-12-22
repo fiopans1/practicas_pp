86d24d197c40f9705075e698a4c5535e3ed23e1b
fiopans1 <dsuaran@gmail.com>
2021-12-22 18:35:57 +0100

    jklfsrfhbnsdjklnklsdjr

--- practica13/asort.ml
+++ practica13/asort.ml
@@ -18,30 +18,31 @@ let rec msort1 ord l = match l with
 
 
 let rec merge ord v izq der cent=
-  let i= (ref izq);let j= (ref (cent+1)); k=(ref izq); let aux=(Array.make (der+1) 0) in
+  let i= (ref izq) in let j= (ref (cent+1)) in  k=(ref izq) in let aux=(Array.make (der+1) 0) in
 while i<= cent && j<= der do (
     if (ord v.((!i)) v.((!j))) then (aux.((!k)) <- v.((!i)) ; i := (!i + 1))
     else aux.((!k)) <- v.((!j)) ; j = (!j + 1) ; k := (!k +1))
 done ;
 while !i <= cent do
   aux.((!k)) <- v.((!i)) ; i := (!i +1) ; k := (!k +1)
-let rec merge ord v izq der cent=
-  let i= (ref izq);let j= (ref (cent+1)); k=(ref izq); let aux=(Array.make (der+1) 0) in
-while i<= cent && j<= der do (
-    if (ord v.((!i)) v.((!j))) then (aux.((!k)) <- v.((!i)) ; i := (!i + 1))
-    else aux.((!k)) <- v.((!j)) ; j = (!j + 1) ; k := (!k +1))
-done ;
 
+
+
+let rec merge ord v izq der cent=
+  let i= (ref izq) in let j= (ref (cent+1)) in let k=(ref izq) in let aux=(Array.make(der+1) v.(0)) in
+while !i<= cent && !j<= der do (
+    if (ord v.((!i)) v.((!j))) then (aux.((!k)) <- v.((!i)) ; i := (!i + 1))
+    else aux.((!k)) <- v.((!j)) ; j := (!j + 1) ; k := (!k +1))
+done ;
 while !i <= cent do
-  aux.((!k)) <- v.((!i)) ; i := (!i + 1) ; k := (!k +1))
+  aux.((!k)) <- v.((!i)) ; i := (!i + 1) ; k := (!k +1)
 done ;
 while !i <= der do
-  aux.((!k)) <- v.((!j)) ; j := (!j + 1) ; k := (!k +1))
+  aux.((!k)) <- v.((!j)) ; j := (!j + 1) ; k := (!k +1)
 done ;
-for k:=izq to der do
-  v.(k) <- aux.(k)
-done ;
- () ;;
+for z = izq to der do
+  v.(z) <- aux.(z)
+done ;;
 
 
 
@@ -76,3 +77,36 @@ let rec merge_sort a p r =
       c := (!c + p)/2
     done
 ;;
+
+
+
+  let rec merge ord v izq der cent=
+    let i= (ref izq) in let j= (ref (cent+1)) in let k=(ref izq) in let aux=(Array.make (Array.length v) v.(0)) in
+    while !i<= cent && !j<= der do (
+        if (ord v.((!i)) v.((!j))) then (aux.((!k)) <- v.((!i)) ; i := (!i + 1))
+        else aux.((!k)) <- v.((!j)) ; j := (!j + 1) ; k := (!k +1))
+    done ;
+    while !i <= cent do
+      aux.((!k)) <- v.((!i)) ; i := (!i + 1) ; k := (!k +1)
+    done ;
+    while !i <= der do
+      aux.((!k)) <- v.((!j)) ; j := (!j + 1) ; k := (!k +1)
+    done ;
+    for z = izq to der do
+      v.(z) <- aux.(z)
+    done ;;
+
+let rec mergesort ord v izq der=
+  let cent= ref der in
+  while izq<der && izq < !cent do
+    mergesort ord v izq ((izq+der)/2);
+    mergesort ord v ((izq+der)/2 +1) der;
+    merge ord v izq (der - 9) ((izq+der)/2);
+    cent := (!cent + izq)/2
+  done;;
+
+let asort ord v=
+  mergesort ord v 0 ((Array.length v) -1);;
+
+let pene= [| 5;2;3;5;67;3;4;56;7;4;9;3;6;8;1;6|];;
+let f =  fun x y -> x<y;;
